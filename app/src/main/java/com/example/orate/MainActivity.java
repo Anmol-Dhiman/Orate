be9e4b4124cbc119a70138a3d4070bfe4ac1272e
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-06-15 02:33:26 +0530

    ui

--- app/src/main/java/com/example/orate/MainActivity.java
+++ app/src/main/java/com/example/orate/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.orate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.orate.Activity.Fragments.CallHistory;
+import com.example.orate.Activity.Fragments.ContactList;
+import com.example.orate.Activity.Fragments.UserProfile;
+import com.example.orate.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity    {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/orate/Repository/model.java
@@ -0,0 +1,5 @@
+package com.example.orate.Repository;
+
+public class model {
+
+}
